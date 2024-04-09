ffa3371e42b56d645a4725e9b5401358b930a5b6
Hayeon LEE <fhdodl@naver.com>
2024-04-09 23:06:46 +0900

    feat : docker 이미지 업로드 테스트

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,7 @@
-# Node.js 20 버전을 베이스 이미지로 사용
-FROM node:20
-
-# 작업 디렉토리 설정
-WORKDIR /usr/src/app
-
-# 애플리케이션 의존성 파일 복사
-COPY package*.json ./
-
-# 의존성 설치
+FROM node:14
+WORKDIR /app
+COPY package.json /app
 RUN npm install
-
-# 애플리케이션 소스 복사
-COPY . .
-
-# 애플리케이션 시작 명령
-CMD [ "node", "app.js" ]
+COPY . /app
+CMD ["node", "index.js"]
+EXPOSE 8080
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const http = require('http');
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
 });
 
-const PORT = process.env.PORT || 8080;
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
